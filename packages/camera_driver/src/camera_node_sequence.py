12e131289eb0b3a268200988dd9d0fde8a9f3892
Aleksandar Petrov <aleksanndar1995@gmail.com>
2019-08-08 21:02:12 +0200

    untested camera node changes

--- packages/camera_driver/src/camera_node_sequence.py
+++ packages/camera_driver/src/camera_node_sequence.py
@@ -24,16 +24,18 @@ class CameraNode(object):
     The node can publish both at high and low framerate settings which are configurable parameters. Publishing to
     the `~framerate_high_switch` topic can switch between the two settings. The high setting is used by default.
 
+    TODO: Check if the dynamic parameter changing actually works
+
     Configuration:
-        ~framerate_high (float): The high setting for the camera framerate, default is 30.0 fps
-        ~framerate_low (float): The low setting for the camera framerate, default is 15.0 fps
+        ~framerate (float): The camera image acquisition framerate, default is 30.0 fps
         ~res_w (int): The desired width of the acquired image, default is 640px
         ~res_h (int): The desired height of the acquired image, default is 480px
+        ~exposure_mode (string): PiCamera exposure mode, one of `these <https://picamera.readthedocs.io/en/latest/api_camera.html?highlight=sport#picamera.PiCamera.exposure_mode>`_, default is `sports`
 
-    Subscriber:
-        ~framerate_high_switch (BoolStamped): If `True` is published to this topic, the framerate
-            will be switched to the `~framerate_high` value. If `False` is published to this topic,
-            the framerate will be switched to the `~framerate_low` value.
+    # Subscriber:
+    #     ~framerate_high_switch (BoolStamped): If `True` is published to this topic, the framerate
+    #         will be switched to the `~framerate_high` value. If `False` is published to this topic,
+    #         the framerate will be switched to the `~framerate_low` value.
 
     Publisher:
         ~image/compressed (CompressedImage): The acquired camera images
@@ -55,18 +57,28 @@ class CameraNode(object):
         self.node_name = rospy.get_name()
         rospy.loginfo("[%s] Initializing......" % (self.node_name))
 
-        self.framerate_high = self.setupParam("~framerate_high", 30.0)
-        self.framerate_low = self.setupParam("~framerate_low", 15.0)
-        self.res_w = self.setupParam("~res_w", 640)
-        self.res_h = self.setupParam("~res_h", 480)
+        # Get the parameters and set the parameter update cycle
+        self.parameters = {'~framerate': None,
+                           '~res_w': None,
+                           '~res_h': None,
+                           '~exposure_mode': None}
+        self.parametersChanged = True
+        self.updateParameters()
+        self.updateParametersTimer = rospy.Timer(period=1.0, callback=self.updateParameters(), oneshot=False)
+
+        # self.framerate_high = self.setupParam("~framerate_high", 30.0)
+        # self.framerate_low = self.setupParam("~framerate_low", 15.0)
+        # self.res_w = self.setupParam("~res_w", 640)
+        # self.res_h = self.setupParam("~res_h", 480)
 
         self.image_msg = CompressedImage()
 
         # Setup PiCamera
         self.camera = PiCamera()
-        self.framerate = self.framerate_high  # default to high
-        self.camera.framerate = self.framerate
-        self.camera.resolution = (self.res_w, self.res_h)
+        # self.framerate = self.framerate_high  # default to high
+        self.camera.framerate = self.parameters['~framerate']
+        self.camera.resolution = (self.parameters['~res_w'], self.parameters['~res_h'])
+        self.camera.exposure_mode = self.parameters['~exposure_mode']
 
         # For intrinsic calibration
         self.cali_file_folder = '/data/config/calibrations/camera_intrinsic/'
@@ -77,10 +89,10 @@ class CameraNode(object):
         self.pub_img = rospy.Publisher("~image/compressed",
                                        CompressedImage,
                                        queue_size=1)
-        self.sub_switch_high = rospy.Subscriber("~framerate_high_switch",
-                                                BoolStamped,
-                                                self.cbSwitchHigh,
-                                                queue_size=1)
+        # self.sub_switch_high = rospy.Subscriber("~framerate_high_switch",
+        #                                         BoolStamped,
+        #                                         self.cbSwitchHigh,
+        #                                         queue_size=1)
 
         # Create service (for camera_calibration)
         self.srv_set_camera_info = rospy.Service("~set_camera_info",
@@ -89,26 +101,26 @@ class CameraNode(object):
         self.stream = io.BytesIO()
 
         self.is_shutdown = False
-        self.update_framerate = False
+        # self.update_framerate = False
 
         rospy.loginfo("[%s] Initialized." % (self.node_name))
 
-    def cbSwitchHigh(self, switch_msg):
-        """Callback for the imagery frequency switch.
-
-        Increases / decreases the frequency of imagery.
-
-        Args:
-            switch_msg (BoolStamped): switch_msg
-        """
-        rospy.loginfo("[%s] frequency switch to %s." % (self.node_name,
-                                                        switch_msg.data))
-        if switch_msg.data and self.framerate != self.framerate_high:
-            self.framerate = self.framerate_high
-            self.update_framerate = True
-        elif not switch_msg.data and self.framerate != self.framerate_low:
-            self.framerate = self.framerate_low
-            self.update_framerate = True
+    # def cbSwitchHigh(self, switch_msg):
+    #     """Callback for the imagery frequency switch.
+    #
+    #     Increases / decreases the frequency of imagery.
+    #
+    #     Args:
+    #         switch_msg (BoolStamped): switch_msg
+    #     """
+    #     rospy.loginfo("[%s] frequency switch to %s." % (self.node_name,
+    #                                                     switch_msg.data))
+    #     if switch_msg.data and self.framerate != self.framerate_high:
+    #         self.framerate = self.framerate_high
+    #         self.update_framerate = True
+    #     elif not switch_msg.data and self.framerate != self.framerate_low:
+    #         self.framerate = self.framerate_low
+    #         self.update_framerate = True
 
     def startCapturing(self):
         """Initialize and closes image stream.
@@ -126,14 +138,21 @@ class CameraNode(object):
                                              splitter_port=0)
             except StopIteration:
                 pass
-            self.camera.framerate = self.framerate
-            self.update_framerate = False
+
+            # Update the camera parameters
+            self.camera.framerate = self.parameters['~framerate']
+            self.camera.resolution = (self.parameters['~res_w'], self.parameters['~res_h'])
+            self.camera.exposure_mode = self.parameters['~exposure_mode']
+            self.parametersChanged = False
 
         self.camera.close()
         rospy.loginfo("[%s] Capture Ended." % (self.node_name))
 
     def grabAndPublish(self, stream, publisher):
         """Captures a frame from stream and publishes it.
+
+            TODO: Description is completely really what it does... it is a IO stream generator...
+            TODO: Does this introduce a delay of 1/fps to the publishing?
 
             If the stream is stable (no updates in frequency or shutdowns),
             grabs a frame, creates the image message and publishes it.
@@ -142,7 +161,7 @@ class CameraNode(object):
                 stream (BytesIO): imagery stream
                 publisher (Publisher): publisher of topic
         """
-        while not (self.update_framerate or self.is_shutdown or rospy.is_shutdown()):
+        while not (self.parametersChanged or self.is_shutdown or rospy.is_shutdown()):
             yield stream
             # Construct image_msg
             # Grab image from stream
@@ -168,25 +187,49 @@ class CameraNode(object):
 
             rospy.sleep(rospy.Duration.from_sec(0.001))
 
-    def setupParam(self, param_name, default_value):
-        """Parameter server handler.
+    # def setupParam(self, param_name, default_value):
+    #     """Parameter server handler.
+    #
+    #         Sets value of parameter and file and prints it.
+    #
+    #         Args:
+    #             param_name (String): name of the parameter
+    #             value(String): value of the paramter
+    #     """
+    #     value = rospy.get_param(param_name, default_value)
+    #     # Write to parameter server for transparancy
+    #     rospy.set_param(param_name, value)
+    #     rospy.loginfo("[%s] %s = %s " % (self.node_name, param_name, value))
+    #     return value
 
-            Sets value of parameter and file and prints it.
+    def updateParameters(self):
+        """Keeping the node parameters up to date with the parameter server.
 
-            Args:
-                param_name (String): name of the parameter
-                value(String): value of the paramter
+        Goes through all the node parameters and check if the value for any of them is changed
+        in the parameter server. If there is a parameter that wasn't found, it will throw an `KeyError`
+        exception.
+
+        If a value of a parameter is changed, it will be updated and `self.parametersChanged` will be set
+        to `True` in order to inform other methods to adjust their behavior.
+
+        Raises:
+            KeyError: if one of the parameters is not found in the parameter server
+
         """
-        value = rospy.get_param(param_name, default_value)
-        # Write to parameter server for transparancy
-        rospy.set_param(param_name, value)
-        rospy.loginfo("[%s] %s = %s " % (self.node_name, param_name, value))
-        return value
+
+        for param_name in self.parameters:
+            new_value = rospy.get_param(param_name)
+            if new_value != self.parameters[param_name]:
+                self.parameters[param_name] = new_value
+                self.parametersChanged = True
+                rospy.loginfo("[%s] Setting parameter %s = %s " % (self.node_name, param_name, new_value))
+
 
     def onShutdown(self):
         """Shutdown procedure."""
         rospy.loginfo("[%s] Closing camera." % (self.node_name))
         self.is_shutdown = True
+        self.updateParametersTimer.shutdown()
         rospy.loginfo("[%s] Shutdown." % (self.node_name))
 
     def cbSrvSetCameraInfo(self, req):
