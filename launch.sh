2b2711a5cf322e2eb1a7c26621bf3e8a5cfa8049
Andrea F Daniele <afdaniele@ttic.edu>
2019-11-07 14:36:36 -0600

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -4,4 +4,8 @@ set -e
 
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
+# read robot type (update PATH)
+ROBOT_TYPE=`cat /data/stats/init_sd_card/parameters/robot_type`
+
+# launch duckiebot_interface
 roslaunch duckiebot_interface all_drivers.launch veh:=$VEHICLE_NAME robot_type:=$ROBOT_TYPE
