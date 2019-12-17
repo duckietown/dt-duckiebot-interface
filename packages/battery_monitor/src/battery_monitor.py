d8bc2d5f46d8f6d4ac6d090a688a1bdcd303da87
Timothy Scott <scottti@student.ethz.ch>
2019-12-17 14:49:31 +0100

    Fetch more data from battery pack

--- packages/battery_monitor/src/battery_monitor.py
+++ packages/battery_monitor/src/battery_monitor.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python
 
 from duckietown import DTROS
-from duckietown_msgs.msg import BatteryStatus
+from sensor_msgs.msg import BatteryState
 import serial
+from serial.serialutil import SerialException
 import re
+import os
+import io
 import rospy
 
 RETRIES = 5
+
+
+class BatteryNotFound(Exception):
+    pass
 
 
 class BatteryMonitor(DTROS):
@@ -17,18 +24,30 @@ class BatteryMonitor(DTROS):
         self.parameters['~device'] = None
         self.updateParameters()
 
-        self.ser = serial.Serial(self.parameters['~device'], baudrate=9600, timeout=0.5)
         # Battery status is printed to serial as, for example, "SOC(%):   82"
         self.regex = re.compile(r"SOC\(%\):\s+(\d+)")
 
-        self.pub = rospy.Publisher("~battery_monitor", BatteryStatus, queue_size=1)
+        self.pub = rospy.Publisher("~battery_monitor", BatteryState, queue_size=1)
 
-        charge = self.read_battery_status()
+        devices = filter(lambda x: x.startswith('ttyACM'), os.listdir('/dev'))
 
-        if charge is None:
-            raise Exception("Unable to communicate with battery on port {}".format(self.parameters['~device']))
+        for device in devices:
+            device = os.path.join("/dev", device)
+            self.ser = serial.Serial(device, baudrate=9600, timeout=2)
+            # self.ser_io = io.TextIOWrapper(
+            #     io.BufferedRWPair(self.ser, self.ser, 1),
+            #     line_buffering=True
+            # )
+            try:
+                self.read_battery_status()
+                self.log("Connecting to battery on {}".format(device))
+                break
+            except BatteryNotFound:
+                self.log("No battery found on {}...".format(device))
+        else:
+            raise BatteryNotFound("No battery found on any USB port. Shutting down battery monitor.")
 
-        self.timer = rospy.Timer(rospy.Duration.from_sec(10), self.read_battery_status)
+        self.timer = rospy.Timer(rospy.Duration.from_sec(0.9), self.read_battery_status)
 
     def read_battery_status(self, event=None):
         """
@@ -36,24 +55,54 @@ class BatteryMonitor(DTROS):
         :param event: Event for using this as a timer callback
         :return: Percentage number, as int, or None if unsuccessful.
         """
-        number = None
-        # Sometimes (often) the battery just returns empty lines or partial lines. Try a few times, so we hopefully
-        # get a successful read eventually.
-        for i in range(0, RETRIES):
-            line = self.ser.readline()
-            # The battery status is including null characters for some reason.
-            line = line.translate(None, b'\x00')
-            line = line.decode('ascii')
-            number = self.interpret_line(line)
-            if number is not None:
-                break
+        self.ser.reset_input_buffer()
+        line = self.ser.readline(16).replace(b'\x00', b' ')
+        percent_remaining = self.interpret_line(line)
+        if percent_remaining is None:
+            raise BatteryNotFound('Did not hear battery sending remaining percentage.')
 
-        if number is not None:
-            msg = BatteryStatus()
-            msg.charge = number / 100
-            self.pub.publish(msg)
+        """
+         - VO: mV
+         - TF: Time to full, minutes
+         - ET: External temperature
+         - CR: Current, mA, negative is discharging
+         - TE: Time to empty, minutes
+        """
+        results = {}
+        for command in [b'VO', b'TF', b'ET', b'CR', b'TE']:
+            self.ser.write(command)
+            # The battery always echos the command on its own line
+            cmd_echo = self.ser.readline(4)
+            if command not in cmd_echo:
+                raise BatteryNotFound('Battery did not respond to command {}'.format(command))
+            # After echoing the command, the battery prints the actual data, padded by random whitespace,
+            # control characters, and null characters. So filter out all the garbage.
+            cmd_response = self.ser.readline(8)
+            number = int(bytes(filter(lambda x: x in b'+-0123456789', cmd_response)))
+            results[command] = number
 
-        return number
+        msg = BatteryState()
+        msg.voltage = results[b'VO'] / 1000.0
+        msg.current = results[b'CR'] / 1000.0
+        msg.charge = float('nan')
+        msg.capacity = float('nan')
+        msg.design_capacity = 10.0
+        msg.percentage = percent_remaining / 100.0
+        if percent_remaining >= 100:
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_FULL
+        elif msg.current == 0:
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_NOT_CHARGING
+        elif msg.current > 0:
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_CHARGING
+        elif msg.current < 0:
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_DISCHARGING
+        else:
+            msg.power_supply_status = BatteryState.POWER_SUPPLY_STATUS_UNKNOWN
+        msg.power_supply_health = BatteryState.POWER_SUPPLY_HEALTH_UNKNOWN
+        msg.power_supply_technology = BatteryState.POWER_SUPPLY_TECHNOLOGY_LIPO
+        msg.present = True
+        msg.cell_voltage = [msg.voltage]
+        self.pub.publish(msg)
 
     def interpret_line(self, line):
         """
@@ -68,7 +117,8 @@ class BatteryMonitor(DTROS):
             result = self.regex.match(line)
             number = result.group(1)
             return int(number)
-        except:
+        # AttributeError if the regex does not match. ValueError if the string is not a valid number
+        except (AttributeError, ValueError):
             return None
 
 
@@ -76,6 +126,7 @@ if __name__ == "__main__":
     try:
         node = BatteryMonitor()
         rospy.spin()
-    except:
+    except BatteryNotFound as e:
+        print(e)
         print("Unable to communicate with battery. "
               "Assuming old battery is being used, and shutting down battery_monitor")
