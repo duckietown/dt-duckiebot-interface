539a62629b8d4b130bc3573967eab57bbcf915b5
Andrea F. Daniele <afdaniele@ttic.edu>
2020-06-26 18:05:02 -0500

    added --wait to launch file as the ROS master will run in another container

--- launch.sh
+++ launch.sh
@@ -5,7 +5,8 @@ set -e
 # YOUR CODE BELOW THIS LINE
 # ----------------------------------------------------------------------------
 
-roslaunch duckiebot_interface all_drivers.launch \
-  veh:="$VEHICLE_NAME" \
-  robot_type:="$ROBOT_TYPE" \
-  robot_configuration:="$ROBOT_CONFIGURATION"
+roslaunch --wait \
+  duckiebot_interface all_drivers.launch \
+    veh:="$VEHICLE_NAME" \
+    robot_type:="$ROBOT_TYPE" \
+    robot_configuration:="$ROBOT_CONFIGURATION"
