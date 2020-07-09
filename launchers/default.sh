5b8c7d287185dada88cddefad4ebed2f95422ec0
Andrea F. Daniele <afdaniele@ttic.edu>
2020-07-09 12:51:35 -0500

    adapted to template v2

---
+++ launchers/default.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+source /environment.sh
+
+# initialize launch file
+dt-launchfile-init
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable CODE_DIR to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+# launching app
+dt-exec roslaunch --wait \
+  duckiebot_interface all_drivers.launch \
+    veh:="$VEHICLE_NAME" \
+    robot_type:="$ROBOT_TYPE" \
+    robot_configuration:="$ROBOT_CONFIGURATION"
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
+
+# wait for app to end
+dt-launchfile-join
