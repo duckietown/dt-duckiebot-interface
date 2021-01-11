e14185eac2d0eaa8b321ed1f9855b9cc88a8e2b2
Andrea F. Daniele <afdaniele@ttic.edu>
2021-01-11 13:41:33 -0600

    finished implementing button driver node

--- packages/button_driver/src/button_driver_node.py
+++ packages/button_driver/src/button_driver_node.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
-import dataclasses
-import time
-from collections import defaultdict
 
+import time
 import rospy
 
 from duckietown_msgs.msg import ButtonEvent as ButtonEventMsg
@@ -10,33 +8,6 @@ from duckietown_msgs.msg import ButtonEvent as ButtonEventMsg
 from duckietown.dtros import DTROS, NodeType, TopicType
 
 from button_driver import ButtonEvent, ButtonDriver
-
-
-@dataclasses.dataclass
-class FullButtonEvent:
-    press_time: float
-    release_time: float
-
-    @property
-    def duration(self):
-        return self.release_time - self.press_time
-
-    def __le__(self, other):
-        return self.duration <= other
-
-    def __ge__(self, other):
-        return self.duration >= other
-
-    def __lt__(self, other):
-        return self.duration < other
-
-    def __gt__(self, other):
-        return self.duration > other
-
-    def __sub__(self, other) -> float:
-        if not isinstance(other, FullButtonEvent):
-            raise ValueError('Expected object of type `FullButtonEvent`')
-        return self.release_time - other.release_time
 
 
 class ButtonDriverNode(DTROS):
@@ -54,7 +25,7 @@ class ButtonDriverNode(DTROS):
         self._led_gpio_pin = rospy.get_param('~led_gpio_pin')
         self._signal_gpio_pin = rospy.get_param('~signal_gpio_pin')
         # create publishers
-        self._sub = rospy.Publisher(
+        self._pub = rospy.Publisher(
             "~event",
             ButtonEventMsg,
             queue_size=1,
@@ -63,60 +34,39 @@ class ButtonDriverNode(DTROS):
         )
         # create button driver
         self._button = ButtonDriver(self._led_gpio_pin, self._signal_gpio_pin, self._event_cb)
-        # create events history
-        self._events = []
+        # create event holder
         self._ongoing_event = None
 
-    def _time_between(self, event1: ButtonEvent, event2: ButtonEvent):
-        if self._events[event2] == 0.0:
-            return time.time()
-        return self._events[event2] - self._events[event1]
-
     def _event_cb(self, event: ButtonEvent):
-        # use the LED to mimic the event
-        self._button.led.set(event.value)
         # create partial event
         if event == ButtonEvent.PRESS:
             # create new partial event
             self._ongoing_event = time.time()
             return
         # this is a RELEASE event
-        # create new full event
         if self._ongoing_event is None:
             # we missed it, well, next time!
             return
-        self._events.append(FullButtonEvent(
-            press_time=self._ongoing_event, release_time=time.time()
-        ))
+        # create new full event
+        duration = time.time() - self._ongoing_event
         # clear ongoing event
         self._ongoing_event = None
-        # need at least two events
-        if len(self._events) <= 1:
-            return
-        # clear history
-        self._events = self._events[-2:]
-        # analyze history of events looking for known patterns
-        evt1, evt2 = self._events[-1], self._events[-2]
-        # - double click
-        dclick_s = self._TIME_DOUBLE_CLICK_S
-        print(evt1 - evt2)
-        if evt1 - evt2 <= 2 * dclick_s and evt1 < dclick_s and evt2 < dclick_s:
-            print('DOUBLE')
+        # analyze event
+        # - single click
+        if duration < 0.5:
+            self._publish(ButtonEventMsg.EVENT_SINGLE_CLICK)
             return
         # - held for 3 secs
-        if self._TIME_HOLD_3S < evt1 < 2 * self._TIME_HOLD_3S:
-            print('HELD_3')
+        if self._TIME_HOLD_3S < duration < 2 * self._TIME_HOLD_3S:
+            self._publish(ButtonEventMsg.EVENT_HELD_3SEC)
             return
         # - held for 10 secs
-        if self._TIME_HOLD_10S < evt1 < 2 * self._TIME_HOLD_10S:
-            print('HELD_10')
+        if self._TIME_HOLD_10S < duration < 2 * self._TIME_HOLD_10S:
+            self._publish(ButtonEventMsg.EVENT_HELD_10SEC)
             return
-        # - single click
-        print('SINGLE')
-        return
 
     def _publish(self, event: int):
-        print(event)
+        self._pub.publish(ButtonEventMsg(event))
 
     def on_shutdown(self):
         if hasattr(self, '_button'):
