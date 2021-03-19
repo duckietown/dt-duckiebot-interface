52d727b56d1b491aa1c210d561e1abd3ba6b8000
Andrea F. Daniele <afdaniele@ttic.edu>
2021-03-19 18:08:32 -0500

    Button LED: ignore status change request when node is shutting down

--- packages/button_driver/include/button_driver/led.py
+++ packages/button_driver/include/button_driver/led.py
@@ -18,20 +18,28 @@ class ButtonLED:
         if not 1 <= gpio_pin <= 40:
             raise ValueError('The pin number must be within the range [1, 40].')
         self._gpio_pin = gpio_pin
+        self._is_shutdown = False
         # configure GPIO pin
         GPIO.setmode(GPIO.BOARD)
         GPIO.setup(self._gpio_pin, GPIO.OUT)
 
     def on(self):
+        if self._is_shutdown:
+            return
         GPIO.output(self._gpio_pin, GPIO.HIGH)
 
     def off(self):
+        if self._is_shutdown:
+            return
         GPIO.output(self._gpio_pin, GPIO.LOW)
 
     def set(self, value: int):
+        if self._is_shutdown:
+            return
         if value not in [0, 1]:
             raise ValueError("LED value can only be on of [0, 1]")
         GPIO.output(self._gpio_pin, value)
 
     def shutdown(self):
+        self._is_shutdown = True
         self.off()
