2d3c70eedf62d9982635d418e50141bf0f7fdfb7
Andrea F. Daniele <afdaniele@ttic.edu>
2021-06-24 10:52:15 -0500

    added HAT driver for DB21A, DB21B

--- packages/hat_driver/include/hat_driver/utils.py
+++ packages/hat_driver/include/hat_driver/utils.py
@@ -8,6 +8,8 @@ _ROBOT_CONFIGURATION_TO_HAT = {
     RobotConfiguration.DB19: HATv2,
     RobotConfiguration.DB20: HATv2,
     RobotConfiguration.DB21M: HATv3,
+    RobotConfiguration.DB21A: HATv3,
+    RobotConfiguration.DB21B: HATv3,
 }
 _DEFAULT_HAT = HATv1
 
