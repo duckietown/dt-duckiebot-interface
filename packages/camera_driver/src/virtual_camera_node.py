64da07bd9cb370aa3df8e8f3b356625b13e13356
Andrea F. Daniele <afdaniele@ttic.edu>
2021-07-30 09:49:22 -0500

    working on virtual camera

---
+++ packages/camera_driver/src/virtual_camera_node.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import os
+
+import rospy
+import numpy as np
+from typing import cast
+
+from camera_driver import AbsCameraNode
+from sensor_msgs.msg import CompressedImage
+
+from dt_duckiematrix_protocols.WorldMessageProto_pb2 import WorldMessageProto
+from dt_duckiematrix_utils.configuration import get_matrix_avatar_name
+from dt_duckiematrix_utils.socket import DuckieMatrixSocket
+
+
+class VirtualCamera(AbsCameraNode):
+    """
+    Handles the imagery on a Virtual robot.
+    """
+
+    def __init__(self):
+        # Initialize the DTROS parent class
+        super(VirtualCamera, self).__init__()
+        # prepare zmq pipeline
+        self._device: DuckieMatrixSocket = DuckieMatrixSocket.create()
+        if self._device is None or not self._device.connected:
+            self.log("[VirtualCamera]: No virtual camera connection established. Exiting.")
+            exit(1)
+        # ---
+        self.log("[VirtualCamera]: Initialized.")
+
+    def process_frame(self, msg: WorldMessageProto):
+        if msg.camera_frame is None:
+            return
+        # image is already JPEG encoded
+        image_msg = CompressedImage()
+        image_msg.header.stamp = rospy.Time.now()
+        image_msg.format = "jpeg"
+        image_msg.data = msg.camera_frame.payload
+        # publish the compressed image
+        self.publish(image_msg)
+
+    def run(self):
+        """ Image capture procedure.
+        """
+        if self._device is None or not self._device.connected:
+            self.logerr('Device was found disconnected')
+            return
+        # run socket thread and join
+        self._device.start()
+        self._device.join()
+        # ---
+        self.loginfo('Camera worker stopped.')
+
+    def setup(self):
+        # setup camera
+        avatar_name = get_matrix_avatar_name()
+        camera_0_topic = os.path.join(avatar_name, "camera_0")
+        self._device.subscribe(camera_0_topic, )
+        pass
+
+    def release(self, force: bool = False):
+        if self._device is not None:
+            self.loginfo('Releasing camera...')
+            self._device.release()
+            self.loginfo('Camera released.')
+        self._device = None
+
+
+if __name__ == '__main__':
+    # initialize the node
+    camera_node = VirtualCamera()
+    camera_node.start()
+    # keep the node alive
+    rospy.spin()
