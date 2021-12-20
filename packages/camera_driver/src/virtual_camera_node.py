ca32a29038f8197d5ecd43808d07c38c3cb85cba
Andrea F. Daniele <afdaniele@ttic.edu>
2021-12-20 17:06:20 -0600

    implemented virtual_camera_node using new duckiematrix API

--- packages/camera_driver/src/virtual_camera_node.py
+++ packages/camera_driver/src/virtual_camera_node.py
@@ -1,18 +1,19 @@
 #!/usr/bin/env python3
 
-import os
 from threading import Condition
 from typing import Optional
 
 import rospy
-
 from camera_driver import AbsCameraNode
 from sensor_msgs.msg import CompressedImage
 
-from dt_duckiematrix_protocols.CameraFrame import CameraFrame
-from dt_duckiematrix_utils.socket import DuckieMatrixSocket
-from dt_duckiematrix_utils.ros import on_duckiematrix_connection_request
-from dt_robot_utils import get_robot_name
+from dt_duckiematrix_messages.CameraFrame import CameraFrame
+from dt_duckiematrix_protocols import Matrix
+from dt_duckiematrix_protocols.robot.features.sensors import Camera
+from dt_duckiematrix_utils.ros import \
+    on_duckiematrix_connection_request, \
+    DuckiematrixLinkDescription
+from dt_robot_utils import get_robot_configuration
 
 
 class VirtualCamera(AbsCameraNode):
@@ -23,26 +24,53 @@ class VirtualCamera(AbsCameraNode):
     def __init__(self):
         # Initialize the DTROS parent class
         super(VirtualCamera, self).__init__()
-        self._device: Optional[DuckieMatrixSocket] = None
-        self._camera_0_topic = os.path.join(get_robot_name(), "camera_0")
+        self._matrix: Optional[Matrix] = None
+        self._device: Optional[Camera] = None
         # register connection setup function
+        self._connection_request: Optional[DuckiematrixLinkDescription] = None
         self._new_connection_request = Condition()
         on_duckiematrix_connection_request(self.on_connection_request)
 
-    def on_connection_request(self, name: str, data_in_uri: str, data_out_uri: str):
-        self.log(f"[VirtualCamera]: Received request to connect to Duckiematrix '{name}'.")
-        # try to clear the current pipeline
-        try:
-            self.release()
-        except BaseException:
-            pass
-        # prepare zmq pipeline
-        self._device = DuckieMatrixSocket(data_in_uri, data_out_uri)
-        if self._device is None or not self._device.connected:
-            self.log("[VirtualCamera]: No virtual camera connection established.")
+    def on_connection_request(self, link: DuckiematrixLinkDescription):
+        self.log(f"[VirtualCamera]: Received request to connect to Duckiematrix '{link.matrix}'.")
+        # store new connection request
+        self._connection_request = link
         # notify node of the new connection
         with self._new_connection_request:
             self._new_connection_request.notify()
+
+    def setup(self):
+        if self._connection_request is None:
+            return
+        # ---
+        link = self._connection_request
+        configuration = get_robot_configuration()
+        # prepare zmq pipeline
+        self._matrix: Matrix = Matrix(link.uri)
+        self._device: Camera = self._matrix.robots.create(configuration.name, link.entity).camera
+        # subscribe to camera topic
+        self._device.attach(self.process_frame)
+        self._device.start()
+
+    def run(self):
+        """ Image capture procedure.
+        """
+        if self._device is None:
+            # wait for connection request
+            self.loginfo('Waiting for connection request...')
+            with self._new_connection_request:
+                self._new_connection_request.wait()
+        # do this forever
+        while True:
+            # start device
+            self.setup()
+            self._device.start()
+            self.log("[VirtualCamera]: Initialized.")
+            with self._new_connection_request:
+                self._new_connection_request.wait()
+            # ---
+            self.loginfo('Camera worker stopped.')
+            self.release()
 
     def process_frame(self, msg: CameraFrame):
         if msg.frame is None:
@@ -57,42 +85,12 @@ class VirtualCamera(AbsCameraNode):
         # publish the compressed image
         self.publish(image_msg)
 
-    def run(self):
-        """ Image capture procedure.
-        """
-        if self._device is None:
-            # wait for connection request
-            self.loginfo('Waiting for connection request')
-            with self._new_connection_request:
-                self._new_connection_request.wait()
-        # do this forever
-        while True:
-            # new request received
-            if self._device is None or not self._device.connected:
-                self.logerr('Device was found disconnected')
-                return
-            # start device
-            self.setup()
-            self._device.start()
-            self.log("[VirtualCamera]: Initialized.")
-            with self._new_connection_request:
-                self._new_connection_request.wait()
-            # ---
-            self.loginfo('Camera worker stopped.')
-
-    def setup(self):
-        if self._device is None:
-            return
-        # subscribe to camera topic
-        self._device.subscribe(self._camera_0_topic, CameraFrame, self.process_frame)
-
     def release(self, force: bool = False):
-        if self._device is not None:
-            self.loginfo('Releasing camera...')
-            # unsubscribe from camera topic
-            self._device.unsubscribe(self._camera_0_topic, self.process_frame)
-            self._device.release()
-            self.loginfo('Camera released.')
+        self.loginfo('Releasing camera...')
+        # unsubscribe from camera topic
+        self._device.detach(self.process_frame)
+        self._device.release()
+        self.loginfo('Camera released.')
         self._device = None
 
 
