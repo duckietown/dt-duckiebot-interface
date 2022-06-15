63045abb45ee3bce785f8546df8bdae60bf63096
Andrea F Daniele <afdaniele@ttic.edu>
2022-06-15 14:18:51 -0500

    Update tof_node.py

--- packages/tof_driver/src/tof_node.py
+++ packages/tof_driver/src/tof_node.py
@@ -1,27 +1,43 @@
 #!/usr/bin/env python3
 
-# TODO: this should be reverted to the version from ENTE
-
 import rospy
+import dataclasses
+import numpy as np
 
 from sensor_msgs.msg import Range
 from std_msgs.msg import Header
 from duckietown_msgs.msg import DisplayFragment
 
-from display_renderer import DisplayROI, PAGE_TOF, REGION_BODY, MonoImageFragmentRenderer
-from display_renderer.text import monospace_screen
+from dt_vl53l0x import \
+    VL53L0X, \
+    Vl53l0xAccuracyMode
 
+from display_renderer import TextFragmentRenderer, DisplayROI, PAGE_TOF, REGION_HEADER, \
+    REGION_BODY, MonoImageFragmentRenderer
+from display_renderer.text import monospace_screen
 from dt_class_utils import DTReminder
 from duckietown.dtros import DTROS, NodeType, TopicType
 
 
-from dt_robot_utils import get_robot_hardware, RobotHardware
-from tof_driver import ToFAccuracy, ToFDriverAbs
+@dataclasses.dataclass
+class ToFAccuracy:
+    mode: Vl53l0xAccuracyMode
+    timing_budget: float
+    max_range: float
+    # the following are taken from the sensor's datasheet
+    min_range: float = 0.05
+    fov: float = np.deg2rad(25)
 
-if get_robot_hardware() == RobotHardware.VIRTUAL:
-    from tof_driver import VirtualToFDriver as ToFDriver
-else:
-    from tof_driver import ToFDriver
+    @staticmethod
+    def from_string(mode: str):
+        ms = 1 / 1000
+        return {
+            "GOOD": ToFAccuracy(Vl53l0xAccuracyMode.GOOD, 33 * ms, 1.2),
+            "BETTER": ToFAccuracy(Vl53l0xAccuracyMode.BETTER, 66 * ms, 1.2),
+            "BEST": ToFAccuracy(Vl53l0xAccuracyMode.BEST, 200 * ms, 1.2),
+            "LONG_RANGE": ToFAccuracy(Vl53l0xAccuracyMode.LONG_RANGE, 33 * ms, 2.0),
+            "HIGH_SPEED": ToFAccuracy(Vl53l0xAccuracyMode.HIGH_SPEED, 20 * ms, 1.2)
+        }[mode]
 
 
 class ToFNode(DTROS):
@@ -41,11 +57,8 @@ class ToFNode(DTROS):
         self._display_fragment_frequency = rospy.get_param('~display_fragment_frequency', 4)
         self._accuracy = ToFAccuracy.from_string(self._mode)
         # create a VL53L0X sensor handler
-        self._sensor: ToFDriverAbs = ToFDriver(self._sensor_name,
-                                               self._accuracy,
-                                               i2c_bus=self._i2c_bus,
-                                               i2c_address=self._i2c_address)
-        self._sensor.setup()
+        self._sensor = VL53L0X(i2c_bus=self._i2c_bus, i2c_address=self._i2c_address)
+        self._sensor.open()
         # create publisher
         self._pub = rospy.Publisher(
             "~range",
@@ -54,17 +67,17 @@ class ToFNode(DTROS):
             dt_topic_type=TopicType.DRIVER,
             dt_help="The distance to the closest object detected by the sensor"
         )
-        # self._display_pub = rospy.Publisher(
-        #     "~fragments",
-        #     DisplayFragment,
-        #     queue_size=1,
-        #     dt_topic_type=TopicType.VISUALIZATION,
-        #     dt_help="Fragments to display on the display"
-        # )
-        # # create screen renderer
-        # self._renderer = ToFSensorFragmentRenderer(self._sensor_name, self._accuracy)
+        self._display_pub = rospy.Publisher(
+            "~fragments",
+            DisplayFragment,
+            queue_size=1,
+            dt_topic_type=TopicType.VISUALIZATION,
+            dt_help="Fragments to display on the display"
+        )
+        # create screen renderer
+        self._renderer = ToFSensorFragmentRenderer(self._sensor_name, self._accuracy)
         # start ranging
-        self._sensor.start()
+        self._sensor.start_ranging(self._accuracy.mode)
         max_frequency = min(self._frequency, int(1.0 / self._accuracy.timing_budget))
         if self._frequency > max_frequency:
             self.logwarn(f"Frequency of {self._frequency}Hz not supported. The selected mode "
@@ -74,13 +87,11 @@ class ToFNode(DTROS):
         self.loginfo(f"Frequency set to {self._frequency}Hz.")
         # create timers
         self.timer = rospy.Timer(rospy.Duration.from_sec(1.0 / max_frequency), self._timer_cb)
-        # self._fragment_reminder = DTReminder(frequency=self._display_fragment_frequency)
+        self._fragment_reminder = DTReminder(frequency=self._display_fragment_frequency)
 
     def _timer_cb(self, _):
         # detect range
         distance_mm = self._sensor.get_distance()
-        if distance_mm is None:
-            return
         # pack observation into a message
         msg = Range(
             header=Header(
@@ -95,46 +106,46 @@ class ToFNode(DTROS):
         )
         # publish
         self._pub.publish(msg)
-        # # publish display rendering (if it is a good time to do so)
-        # if self._fragment_reminder.is_time():
-        #     self._renderer.update(distance_mm)
-        #     msg = self._renderer.as_msg()
-        #     self._display_pub.publish(msg)
+        # publish display rendering (if it is a good time to do so)
+        if self._fragment_reminder.is_time():
+            self._renderer.update(distance_mm)
+            msg = self._renderer.as_msg()
+            self._display_pub.publish(msg)
 
     def on_shutdown(self):
         # noinspection PyBroadException
         try:
-            self._sensor.release()
+            self._sensor.stop_ranging()
         except BaseException:
             pass
 
 
-# class ToFSensorFragmentRenderer(MonoImageFragmentRenderer):
-#
-#     def __init__(self, name: str, accuracy: ToFAccuracy):
-#         super(ToFSensorFragmentRenderer, self).__init__(
-#             f'__tof_{name}__',
-#             page=PAGE_TOF,
-#             region=REGION_BODY,
-#             roi=DisplayROI(0, 0, REGION_BODY.width, REGION_BODY.height)
-#         )
-#         self._name = name
-#         self._accuracy = accuracy
-#         name = self._name.replace('_', ' ').title()
-#         self._title_h = 12
-#         self._title = monospace_screen(
-#             (self._title_h, self.roi.w), f"ToF / {name}:", scale='vfill'
-#         )
-#
-#     def update(self, measurement_mm: float):
-#         pretty_measurement = f" {(measurement_mm / 10):.1f}cm " \
-#             if (measurement_mm / 1000) < self._accuracy.max_range else "Out-Of-Range"
-#         reading = monospace_screen(
-#             (self.roi.h - self._title_h, self.roi.w),
-#             pretty_measurement, scale='hfill', align='center'
-#         )
-#         self.data[:self._title_h, :] = self._title
-#         self.data[self._title_h:, :] = reading
+class ToFSensorFragmentRenderer(MonoImageFragmentRenderer):
+
+    def __init__(self, name: str, accuracy: ToFAccuracy):
+        super(ToFSensorFragmentRenderer, self).__init__(
+            f'__tof_{name}__',
+            page=PAGE_TOF,
+            region=REGION_BODY,
+            roi=DisplayROI(0, 0, REGION_BODY.width, REGION_BODY.height)
+        )
+        self._name = name
+        self._accuracy = accuracy
+        name = self._name.replace('_', ' ').title()
+        self._title_h = 12
+        self._title = monospace_screen(
+            (self._title_h, self.roi.w), f"ToF / {name}:", scale='vfill'
+        )
+
+    def update(self, measurement_mm: float):
+        pretty_measurement = f" {(measurement_mm / 10):.1f}cm " \
+            if (measurement_mm / 1000) < self._accuracy.max_range else "Out-Of-Range"
+        reading = monospace_screen(
+            (self.roi.h - self._title_h, self.roi.w),
+            pretty_measurement, scale='hfill', align='center'
+        )
+        self.data[:self._title_h, :] = self._title
+        self.data[self._title_h:, :] = reading
 
 
 if __name__ == '__main__':
