db21f3c6999b389f3127922ab5f848cd7d13a550
Jason Hu <jasonhu515@gmail.com>
2023-05-19 10:57:38 +0200

    camera hw test stream instead of grab img msg

--- packages/camera_driver/include/hardware_test_camera/hardware_test_camera.py
+++ packages/camera_driver/include/hardware_test_camera/hardware_test_camera.py
@@ -2,7 +2,6 @@ import base64
 import rospy
 
 from std_srvs.srv import Trigger
-from sensor_msgs.msg import CompressedImage
 from dt_duckiebot_hardware_tests import HWTest, HWTestJsonParamType
 
 
@@ -17,16 +16,10 @@ class HWTestCamera(HWTest):
         return "Camera"
 
     def test_desc_preparation(self) -> str:
-        return self.html_util_ul([
-            "Point the camera to a place with no confidential data, as we will take a picture during the test.",
-            "The picture taken will not be stored anywhere.",
-        ])
+        return self.html_util_ul(["Put the Duckiebot within your reach, as you need to move it."])
 
     def test_desc_expectation(self) -> str:
-        return self.html_util_ul([
-            "Run the test a few times, with slightly different Duckiebot headings.",
-            "The pictures taken should correspond to the Duckiebot's view change",
-        ])
+        return self.html_util_ul(["A live camera stream will be shown below."])
 
     def test_desc_log_gather(self) -> str:
         return self.html_util_ul([
@@ -37,27 +30,21 @@ class HWTestCamera(HWTest):
 
     def _tst(self, _):
         rospy.loginfo(f"[{self.test_id()}] Test service called.")
-        success = True
-        response = ""
-        response_type = HWTestJsonParamType.BASE64
     
-        # Subscribe to the topic and get one message
-        try:
-            msg = rospy.wait_for_message('~image/compressed', CompressedImage, timeout=5.0)
-            base64_encoded_msg = base64.b64encode(msg.data).decode('utf-8')
-            response = base64_encoded_msg
-        except rospy.ROSException as e:
-            rospy.logerr(f"[{self.test_id()}] Experienced error: {e}")
-            success = False
+        instructions = self.html_util_ul([
+            "Move you Duckiebot for a few seconds, and check the stream corresponds to that.",
+        ])
 
         # Return the service response
-        return self.format_response_object(
-            success=success,
+        return self.format_response_stream(
+            success=True,  # does not matter here
+            test_topic_name=f"camera_node/image/compressed",
+            test_topic_type="sensor_msgs/CompressedImage",
             lst_blocks=[
                 self.format_obj(
-                    key="Image",
-                    value_type=response_type,
-                    value=response,
+                    key="Instructions",
+                    value_type=HWTestJsonParamType.HTML,
+                    value=instructions,
                 ),
-            ]
+            ],
         )
