61f8f3b5891b5132db0b7522c765ffa6f04c2333
Jason Hu <jasonhu515@gmail.com>
2023-05-22 20:42:13 +0200

    improve ros node bag download api

--- packages/robot_http_api/include/dt_robot_rest_api/actions/logs/__init__.py
+++ packages/robot_http_api/include/dt_robot_rest_api/actions/logs/__init__.py
@@ -4,8 +4,10 @@ import socket
 import traceback
 from datetime import datetime
 
-from flask import Blueprint, send_file, send_from_directory
-from dt_robot_rest_api.utils import response_ok, response_not_found
+from flask import Blueprint, send_file
+from dt_robot_rest_api.utils import (
+    response_ok, response_not_found, response_error,
+)
 
 
 logs_bp = Blueprint("logs", __name__)
@@ -16,36 +18,44 @@ _ROS_LOGS_DIR = "/tmp/log/latest"
 
 @logs_bp.route("/logs/list")
 def search_ros_node_logs():
-    file_pattern = os.path.join(_ROS_LOGS_DIR, f"{_ROBOT_HOSTNAME}-*.log")
+    """Returns a mapping between ros nodes and their log file paths"""
+    try:
+        file_pattern = os.path.join(_ROS_LOGS_DIR, f"{_ROBOT_HOSTNAME}-*.log")
+        log_files = glob.glob(file_pattern)
+        # for log_f in log_files:
+        #     print("Matching log:", log_f)
+
+        node_names = [os.path.basename(f_path).split('-')[1] for f_path in log_files]
+        node_to_log = dict(zip(node_names, log_files))
+
+        ret_data = {
+            "number_of_logs_found": len(log_files),
+            "dict_log_files": node_to_log,
+            "search_pattern": file_pattern
+        }
+
+        return response_ok(ret_data)
+    except Exception as e:
+        traceback.print_exc()
+        return response_error()
+
+
+@logs_bp.route("/logs/download/<nodename>")
+def download_ros_node_logs(nodename):
+    file_pattern = os.path.join(_ROS_LOGS_DIR, f"{_ROBOT_HOSTNAME}-{nodename}-*.log")
     log_files = glob.glob(file_pattern)
-    # for log_f in log_files:
-    #     print("Matching log:", log_f)
-    log_files = [os.path.basename(f_path) for f_path in log_files]
 
-    ret_data = {
-        "n_logs_found": len(log_files),
-        "lst_log_file_names": log_files,
-        "search_pattern": file_pattern
-    }
+    # lex order's last log file, in case there are rolling/truncated logs
+    file_path = sorted(log_files, reverse=True)[0]
 
-    return response_ok(
-        {
-            "result_data": ret_data,
-        },
-    )
-
-
-@logs_bp.route("/logs/download/<filename>")
-def download_ros_node_logs(filename):
-    file_path = os.path.join(_ROS_LOGS_DIR, filename)
     if os.path.exists(file_path):
-        f_base, f_ext = os.path.splitext(filename)
+        # prepare to rename file when downloading
+        f_base, f_ext = os.path.splitext(os.path.basename(file_path))
         # timestamp
         current_time = datetime.now().replace(microsecond=0)
         formatted_time = current_time.isoformat().replace(':', '').replace('-', '').replace('.', '')
         # new filename when downloading
         new_file_name = f"{f_base}-TS{formatted_time}{f_ext}"
-        # print("Sending file as attachment", new_file_name)
         try:
             return send_file(
                 file_path,
