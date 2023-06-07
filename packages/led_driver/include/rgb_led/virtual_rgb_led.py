ffef6b83f9412328bed2400023bc90f1ce868b0e
davide <Tuxliri@users.noreply.github.com>
2023-06-07 11:27:19 -0500

    Fixed bugs in virtual led driver

--- packages/led_driver/include/rgb_led/virtual_rgb_led.py
+++ packages/led_driver/include/rgb_led/virtual_rgb_led.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 
-from threading import Condition
 from typing import List, Optional
 
 from dt_duckiematrix_protocols.robot.features.lights import Lights
@@ -8,6 +7,7 @@ from dt_duckiematrix_utils.ros import \
     on_duckiematrix_connection_request, \
     DuckiematrixLinkDescription
 from dt_robot_utils import get_robot_configuration
+from dt_duckiematrix_protocols.robot.robots import DifferentialDriveRobot
 
 from dt_duckiematrix_protocols import Matrix
 
@@ -47,30 +47,47 @@ class VirtualRGBLED(object):
         self._device: Optional[Lights] = None
         # register connection setup function
         self._connection_request: Optional[DuckiematrixLinkDescription] = None
-        self._new_connection_request = Condition()
         on_duckiematrix_connection_request(self.on_connection_request)
     
     def on_connection_request(self, link: DuckiematrixLinkDescription):
-        self.loginfo(f"[VirtualLEDs]: Received request to connect to Duckiematrix '{link.matrix}'.")
+        class_name = self.__class__.__name__
         # store new connection request
         self._connection_request = link
-        # notify node of the new connection
-        with self._new_connection_request:
-            self._new_connection_request.notify()
+        print(f"[{class_name}]: executing connection request")
+        # Setup the connection to the LEDs
+        self.release()
+        print(f"[{class_name}]: Pre-setup")
+        self.setup()
+        print(f"[{class_name}]: Post-setup")
+
 
     def setup(self):
         if self._connection_request is None:
             print("Not connecting")
             return
         # ---
+        class_name = self.__class__.__name__
+        print(f"[{class_name}] Executing virtual led setup: {self._connection_request}")
         link = self._connection_request
         configuration = get_robot_configuration()
         # create connection to the matrix engine
         self._matrix: Matrix = Matrix(link.uri,auto_commit=True)
+        print(f"[{class_name}] connected to Matrix at {link.uri}")
         # create connection to the vehicle
-        self.robot = self._matrix.robots.create(configuration.name, link.entity)
-        print(f"VIRTUAL LED CONNECTING TO:{self.robot}")
-    
+        robot: DifferentialDriveRobot = self._matrix.robots.create(configuration.name, link.entity)
+        self._device : Lights = robot.lights
+        print(f"[{class_name}] Connected to {link.entity}")
+
+        # convert lights dictionary to an indexed list
+        self.lights_list : List = [
+            self._device.light0,
+            self._device.light1,
+            self._device.light2,
+            self._device.light3,
+            self._device.light4,
+        ]
+        print(f"[{class_name}] Obtained lights list with {len(self.lights_list)} LEDs")
+        
     def setLEDBrightness(self, led, offset, brightness):
         """Used in physical robot led driver
 
@@ -92,42 +109,33 @@ class VirtualRGBLED(object):
             led (:obj:`int`): Index of specific LED (from the table above)
             color (:obj:`list` of :obj:`float`): Brightness for the three RGB channels, in interval [0,1]
         """
-        return
-        # Setup the connection to the LEDs
-        self.setup()
-        # convert lights dictionary to an indexed list
-        self.lights_list = self.get_lights_list()
 
         light_key = f"light{led}"
 
-        with self.robot.lights.atomic():
-            light = getattr(self.robot.lights,light_key)
+        if self._device is not None:
+            with self._device.atomic():
+                light = getattr(self._device,light_key)
 
-            light.color.r = int(color[0] * 255)
-            light.color.g = int(color[1] * 255)
-            light.color.b = int(color[2] * 255)
+                light.color.r = int(color[0] * 255)
+                light.color.g = int(color[1] * 255)
+                light.color.b = int(color[2] * 255)
+        else:
+            print("The LEDs have not yet been connected")
 
-    def __del__(self):
+    def release(self):
         """Destructor method.
 
         Turns off all the LEDs and deletes the PWM object.
 
         """
-        with self.robot.lights.atomic():
-            for light in self.lights_list:
-                light.color.r = 0
-                light.color.g = 0
-                light.color.b = 0
+        if self._device is not None:
+            with self._device.atomic():
+                for light in self.lights_list:
+                    light.color.r = 0
+                    light.color.g = 0
+                    light.color.b = 0
 
-        del self.lights_list
+        self._device = None
     
-    def get_lights_list(self) -> List:
-        lights_list = [
-            self.robot.lights.light0,
-            self.robot.lights.light1,
-            self.robot.lights.light2,
-            self.robot.lights.light3,
-            self.robot.lights.light4,
-        ]
-
-        return lights_list
+    def __del__(self):
+        self.release()
