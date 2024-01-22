daffa9c2fdf1f3d389003b69b43f7427d505e0c3
tuxliri <dvde.iafrate98@gmail.com>
2024-01-22 18:49:33 +0100

    DTSW-5076 Cleaned up entrypoint devices' permissions script

--- assets/entrypoint.d/set-devices-permissions.sh
+++ assets/entrypoint.d/set-devices-permissions.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-USER="duckie"
 # Find the first file with the specified prefix
 first_file=$(find /dev/i2c*  -print -quit)
 
@@ -8,12 +7,11 @@ if [ -n "$first_file" ]; then
     echo "First i2c device found: $first_file"
     # Determine the GID of the 'i2c' group dynamically
     I2C_GID=$(stat -c %g "$first_file")
-    groupmod -g $I2C_GID i2c
     # Add the user to the 'i2c' group with the correct GID
-    usermod -aG $I2C_GID "$USER"
+    usermod -aG $I2C_GID "$DT_USER_NAME"
 else
     echo "No i2c devices found."
 fi
 
 # Fix permissions for serial devices
-usermod -aG dialout "$USER"
+usermod -aG dialout "$DT_USER_NAME"
