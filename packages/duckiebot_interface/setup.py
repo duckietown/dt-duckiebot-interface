50221572a1a7a1d34c200203a43914c49248063a
Andrea F. Daniele <afdaniele@duckietown.com>
2024-02-23 22:37:34 -0500

    updated duckiebot-interface meta-package

---
+++ packages/duckiebot_interface/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+package_name = 'duckiebot_interface'
+packages = ['duckiebot_interface']
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=packages,
+    package_dir={"": "include"},
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Andrea F. Daniele',
+    maintainer_email='afdaniele@duckietown.com',
+    tests_require=['pytest'],
+    entry_points={},
+)
+
