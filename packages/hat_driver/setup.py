2bb7737b94923d1cd4c1d7daca7f8ab6c699e8b3
Andrea F. Daniele <afdaniele@duckietown.com>
2024-02-24 00:16:35 -0500

    hat_driver colcon package

--- packages/hat_driver/setup.py
+++ packages/hat_driver/setup.py
@@ -1,11 +1,21 @@
-## ! DO NOT MANUALLY INVOKE THIS setup.py, USE CATKIN INSTEAD
-from distutils.core import setup
-from catkin_pkg.python_setup import generate_distutils_setup
+from setuptools import setup
 
-# fetch values from package.xml
-setup_args = generate_distutils_setup(
-    packages=["hat_driver"],
+package_name = 'hat_driver'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
     package_dir={"": "include"},
+    data_files=[
+        ('share/ament_index/resource_index/packages', [f'resource/{package_name}']),
+        (f'share/{package_name}', ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Andrea F. Daniele',
+    maintainer_email='afdaniele@duckietown.com',
+    tests_require=['pytest'],
+    entry_points={},
 )
 
-setup(**setup_args)
