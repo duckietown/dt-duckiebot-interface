61aa7ca341662aceed0c6b227f99a507820c6357
Andrea F. Daniele <afdaniele@duckietown.com>
2024-03-06 21:37:09 -0500

    updated camera launcher

--- launchers/sensor-camera.sh
+++ launchers/sensor-camera.sh
@@ -9,7 +9,7 @@ source /environment.sh
 # NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
 # NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
 
-CONFIG_FILE="${ROBOT_HARDWARE}/${ROBOT_TYPE}"
+CONFIG_FILE="${ROBOT_TYPE}/${ROBOT_HARDWARE}/default"
 
 exec python3 \
   -m camera_driver_node.${ROBOT_HARDWARE} \
