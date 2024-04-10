3f510639ec85c5da7b0c82eedf587fc7ba56a1d5
tuxliri <dvde.iafrate98@gmail.com>
2024-04-10 14:08:31 +0200

    Added betaflight SITL startup to flight-controller launcher

--- launchers/flight-controller.sh
+++ launchers/flight-controller.sh
@@ -13,7 +13,8 @@ CONFIG_FILE="default"
 CONFIG_FILE="${ROBOT_HARDWARE}/default"
 
 # Set owner of /data/config/nodes to the current user
-
+dt-launcher-betaflight-sitl&
+sleep 1
 sudo mkdir -p /data/config/nodes
 sudo chown -R ${USER}:${USER} /data/config/nodes
 exec python3 \
