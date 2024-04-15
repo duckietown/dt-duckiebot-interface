0641a39e26b0507d9e22fbbcc6e44d82999f4630
Andrea F. Daniele <afdaniele@duckietown.com>
2024-04-14 23:59:21 -0400

    bug fix with i2c address group permissions

--- assets/entrypoint.d/01.set-devices-permissions.sh
+++ assets/entrypoint.d/01.set-devices-permissions.sh
@@ -6,6 +6,10 @@ first_file=$(find /dev/ -maxdepth 1 -name 'i2c*' -print -quit)
 if [ -n "$first_file" ]; then
     # Determine the GID of the 'i2c' group dynamically
     I2C_GID=$(stat -c %g "$first_file")
+    # create the 'i2c' group if it doesn't exist
+    set +e
+    getent group $I2C_GID &>/dev/null || groupmod -g $I2C_GID i2c
+    set -e
     # Add the user to the 'i2c' group with the correct GID
     usermod -aG $I2C_GID "$DT_USER_NAME"
 else
