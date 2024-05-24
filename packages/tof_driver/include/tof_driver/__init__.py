92ee3fcdf1a90914db42f27d73c8575a16f0a7e6
tuxliri <dvde.iafrate98@gmail.com>
2024-05-24 17:44:17 +0200

    Added support for VL53L1X distance sensor

--- packages/tof_driver/include/tof_driver/__init__.py
+++ packages/tof_driver/include/tof_driver/__init__.py
@@ -6,13 +6,17 @@ for talking to a single wheel encoder.
 
 """
 
-from dt_robot_utils import get_robot_hardware, RobotHardware, get_robot_type, RobotType
+from dt_robot_utils import get_robot_hardware, RobotHardware, get_robot_type, RobotType, RobotConfiguration
+from dt_robot_utils.robot import get_robot_configuration
 
 from .tof_driver_abs import ToFAccuracy, ToFDriverAbs
 
 if get_robot_hardware() != RobotHardware.VIRTUAL:
     # this breaks if imported when running on a virtual robot
-    from .tof_driver import ToFDriver
+    if get_robot_configuration() == RobotConfiguration.DD24:
+        from .tof_driver import ToFDriverVL53L1X as ToFDriver
+    else:
+        from .tof_driver import ToFDriverVL53L0X as ToFDriver
 else:
     if get_robot_type() == RobotType.DUCKIEDRONE:
         from .gazebo_tof_driver import GazeboToFDriver as ToFDriver
