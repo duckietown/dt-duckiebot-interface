17f05f3bc3f2c2ba31c6a6b5ccf9922cf1463569
tuxliri <dvde.iafrate98@gmail.com>
2024-06-21 11:50:34 +0200

    Added DTPS camera node for raspberry pi arm64v8

--- packages/camera_driver/include/camera_driver_node/raspberry_pi_64.py
+++ packages/camera_driver/include/camera_driver_node/raspberry_pi_64.py
@@ -1,58 +1,56 @@
 #!/usr/bin/env python3
 
-import cv2
-import rospy
 import atexit
 import subprocess
+import cv2
+import asyncio
+import argparse
 import numpy as np
-from typing import cast
-
-from camera_driver import AbsCameraNode
-from sensor_msgs.msg import CompressedImage
-
-from duckietown.dtros import DTParam, ParamType
 
 
-class RaspberryPi64Camera(AbsCameraNode):
+from camera_driver import CameraNodeAbs
+
+class CameraNode(CameraNodeAbs):
     """
-    Handles the imagery on a Raspberry Pi 64.
+    Handles the imagery on a Raspberry Pi.
     """
-
     VIDEO_DEVICE = "/dev/video0"
 
-    def __init__(self):
+    def __init__(self, config: str, sensor_name: str):
         # Initialize the DTROS parent class
-        super(RaspberryPi64Camera, self).__init__()
+        super(CameraNode, self).__init__(config, sensor_name)
         # prepare gstreamer pipeline
         self._device = None
-        # exposure
-        self._exposure = DTParam(
-            "~exposure", param_type=ParamType.INT, help="Camera exposure (v4l2)"
-        )
-        # ---
-        self.log("[RaspberryPi64Camera]: Initialized.")
+        self.loginfo("[CameraNode]: Initialized.")
 
-    def run(self):
-        """Image capture procedure.
+    async def worker(self):
+        """
+        Image capture procedure.
 
         Captures a frame from the /dev/video0 image sink and publishes it.
         """
         if self._device is None or not self._device.isOpened():
             self.logerr("Device was found closed")
             return
+        # init queues
+        await self.dtps_init_queues()
         # get first frame
         retval, image = self._device.read() if self._device else (False, None)
+
         # keep reading
-        while (not self.is_stopped) and (not self.is_shutdown) and retval:
+        while not self.is_shutdown:
+            if not retval:
+                self.logerr("Could not read image from camera")
+                await asyncio.sleep(1)
+                continue
             if image is not None:
-                # image is already JPEG encoded
-                image = cast(np.ndarray, image)
-                image_msg = CompressedImage()
-                image_msg.header.stamp = rospy.Time.now()
-                image_msg.format = "jpeg"
-                image_msg.data = image.tobytes()
-                # publish the compressed image
-                self.publish(image_msg)
+                # without HW acceleration, the image is returned as RGB, encode on CPU
+                jpeg: bytes = np.uint8(image).tobytes()
+
+                # publish
+                await self.publish(jpeg)
+            # return control to the event loop
+            await asyncio.sleep(0.001)
             # grab next frame
             retval, image = self._device.read() if self._device else (False, None)
         self.loginfo("Camera worker stopped.")
@@ -64,7 +62,7 @@ class RaspberryPi64Camera(AbsCameraNode):
         }
         for key in cam_props:
             subprocess.call(
-                f"v4l2-ctl -d {RaspberryPi64Camera.VIDEO_DEVICE} -c {key}={str(cam_props[key])}", shell=True
+                f"v4l2-ctl -d {CameraNode.VIDEO_DEVICE} -c {key}={str(cam_props[key])}", shell=True
             )
         # create VideoCapture object
         if self._device is None:
@@ -72,7 +70,7 @@ class RaspberryPi64Camera(AbsCameraNode):
         # open the device
         if not self._device.isOpened():
             try:
-                self._device.open(RaspberryPi64Camera.VIDEO_DEVICE, cv2.CAP_V4L2)
+                self._device.open(CameraNode.VIDEO_DEVICE, cv2.CAP_V4L2)
                 # make sure the device is open
                 if not self._device.isOpened():
                     msg = "OpenCV cannot open camera"
@@ -80,16 +78,16 @@ class RaspberryPi64Camera(AbsCameraNode):
                     raise RuntimeError(msg)
                 # configure camera
                 self._device.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc(*"MJPG"))
-                self._device.set(cv2.CAP_PROP_FRAME_WIDTH, self._res_w.value)
-                self._device.set(cv2.CAP_PROP_FRAME_HEIGHT, self._res_h.value)
-                self._device.set(cv2.CAP_PROP_FPS, self._framerate.value)
-                self._device.set(cv2.CAP_PROP_CONVERT_RGB, False)
+                self._device.set(cv2.CAP_PROP_FRAME_WIDTH, self.configuration.res_w)
+                self._device.set(cv2.CAP_PROP_FRAME_HEIGHT, self.configuration.res_h)
+                self._device.set(cv2.CAP_PROP_FPS, self.configuration.framerate)
+                self._device.set(cv2.CAP_PROP_CONVERT_RGB, 0.0)
                 # Set auto exposure to false
-                if self._exposure_mode.value == "sports":
+                if self.configuration.exposure_mode == "sports":
                     msg = "Setting exposure to 'sports' mode."
                     self.loginfo(msg)
                     self._device.set(cv2.CAP_PROP_AUTO_EXPOSURE, 0.75)
-                    self._device.set(cv2.CAP_PROP_EXPOSURE, self._exposure.value)
+                    self._device.set(cv2.CAP_PROP_EXPOSURE, self.configuration.exposure)
                 # try getting a sample image
                 retval, _ = self._device.read()
                 if not retval:
@@ -104,6 +102,17 @@ class RaspberryPi64Camera(AbsCameraNode):
             # register self.close as cleanup function
             atexit.register(self.stop)
 
+    def on_shutdown(self):
+        if self._device is not None:
+            self._device.release()
+        self.loginfo("OpenCV device released.")
+
+    def stop(self):
+        """
+        docstring
+        """
+        self.release()
+
     def release(self, force: bool = False):
         if self._device is not None:
             self.loginfo("Releasing camera...")
@@ -116,9 +125,16 @@ class RaspberryPi64Camera(AbsCameraNode):
         self._device = None
 
 
+def main():
+    parser: argparse.ArgumentParser = argparse.ArgumentParser()
+    parser.add_argument("--sensor-name", type=str, required=True, help="Name of the sensor")
+    parser.add_argument("--config", type=str, required=True, help="Name of the configuration")
+    args: argparse.Namespace = parser.parse_args()
+    # create node
+    node: CameraNode = CameraNode(config=args.config, sensor_name=args.sensor_name)
+    # launch the node
+    node.spin()
+
+
 if __name__ == "__main__":
-    # initialize the node
-    camera_node = RaspberryPi64Camera()
-    camera_node.start()
-    # keep the node alive
-    rospy.spin()
+    main()
