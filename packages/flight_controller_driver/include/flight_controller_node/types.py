0bb8c41f2d5a9dd8b32e8baf2607b74768846681
Davide Iafrate <dvde.iafrate98@gmail.com>
2024-09-02 18:21:32 +0200

    Moving flight controller node to mavsdk

--- packages/flight_controller_driver/include/flight_controller_node/types.py
+++ packages/flight_controller_driver/include/flight_controller_node/types.py
@@ -1,11 +1,35 @@
 from dataclasses import dataclass
+from enum import IntEnum
 from typing import List, Optional, Tuple
 
-from flight_controller_driver.flight_controller_abs import SerialConfig
-from flight_controller_driver.flight_controller_virtual import SITLConfig
-from flight_controller_driver.flight_controller_physical import Device
+from serial import SerialException
+
 
 from dt_node_utils.config import NodeConfiguration
+
+class FCError(SerialException):
+    pass
+
+class DroneMode(IntEnum):
+    DISARMED = 0
+    ARMED = 1
+    FLYING = 2
+
+@dataclass
+class SerialConfig:
+    port: str
+    baudrate: Optional[int] = 115200
+
+@dataclass
+class Device:
+    vendor_id: str
+    product_id: str
+
+@dataclass
+class SITLConfig:
+    ip: str
+    msp_port: int
+    rc_port: int
 
 @dataclass
 class FlightControllerConfiguration(NodeConfiguration):
