126105fa4cabb1c19974548f6a69b3df1be0faa9
liammcalpineduckietown <liam.mcalpine@duckietown.com>
2025-04-03 13:25:44 +0200

    control mapper added

---
+++ launchers/control-mapper.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+source /environment.sh
+
+# YOUR CODE BELOW THIS LINE
+# ----------------------------------------------------------------------------
+
+
+# NOTE: Use the variable DT_PROJECT_PATH to know the absolute path to your code
+# NOTE: Use `dt-exec COMMAND` to run the main process (blocking process)
+
+exec python3 -m control_mapper_node.main
+
+
+# ----------------------------------------------------------------------------
+# YOUR CODE ABOVE THIS LINE
