46476100e57e86748000220f1b85e451a02a3abf
Davide iafrate <davide.iafrate@duckietown.com>
2026-02-12 06:53:40 +0000

    fix: update GPIO library import to support Jetson Orin Nano

--- packages/hat_driver/include/hat_driver/motor.py
+++ packages/hat_driver/include/hat_driver/motor.py
@@ -14,7 +14,7 @@ ROBOT_HARDWARE = get_device_hardware_brand()
 
 if ROBOT_HARDWARE in [DeviceHardwareBrand.RASPBERRY_PI, DeviceHardwareBrand.RASPBERRY_PI_64]:
     import RPi.GPIO as GPIO
-elif ROBOT_HARDWARE == DeviceHardwareBrand.JETSON_NANO:
+elif ROBOT_HARDWARE == DeviceHardwareBrand.JETSON_NANO or ROBOT_HARDWARE == DeviceHardwareBrand.JETSON_ORIN_NANO:
     import Jetson.GPIO as GPIO
 else:
     raise ValueError(f"Hardware `{ROBOT_HARDWARE.name}` not supported.")
