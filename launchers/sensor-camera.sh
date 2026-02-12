800aa7b52a7537b78815b578a540d882fe9a6370
Davide iafrate <davide.iafrate@duckietown.com>
2026-02-12 05:32:11 +0000

    fix: adjust ROBOT_HARDWARE for Jetson Orin Nano to use Jetson Nano driver

--- launchers/sensor-camera.sh
+++ launchers/sensor-camera.sh
@@ -12,6 +12,11 @@ source /environment.sh
 SENSOR_NAME="front_center"
 CONFIG_FILE="${ROBOT_TYPE}/${ROBOT_HARDWARE}/${SENSOR_NAME}/default"
 
+# The jetson orin nano uses the same camera driver as the jetson nano 
+if [ "$ROBOT_HARDWARE" = "jetson_orin_nano" ]; then
+  ROBOT_HARDWARE="jetson_nano"
+fi
+
 exec python3 \
   -m camera_driver_node.${ROBOT_HARDWARE} \
     --sensor-name ${SENSOR_NAME} \
